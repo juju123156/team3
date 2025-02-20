15c62e5eb365ffe96f8de9470b3d15ace8ddccb1
juju123156 <fnqllove11@naver.com>
2025-02-20 19:08:40 +0900

    day3

--- docker_build.sh
+++
@@ -1,7 +0,0 @@
-prefix="group1-team3"
-app="accounts-service-by-jenkins"
-ver="0.2.20"
-
-echo "docker build -t k8s-vga-worker1:5000/${prefix}-${app}:v${ver}"
-docker build --platform=linux/amd64 -t k8s-vga-worker1:5000/${prefix}-${app}:v${ver} ./
-#docker push k8s-vga-worker1:5000/${prefix}-${app}:v${ver}
